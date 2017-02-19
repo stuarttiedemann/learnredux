f8a6d21803709fa0eff938c13ee48bd25e145546
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-02-19 16:50:14 -0600

    added redux-todo-example

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -23,4 +23,13 @@ console.log('Starting redux');
 // console.log(startingValue);
 // console.log(res);
 
-var store = redux.createStore();
+var reducer = (state = {name: 'Anonymous'}, action) => {
+    // state = state || {name: 'Anonymous'};
+
+    return state;
+};
+
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
